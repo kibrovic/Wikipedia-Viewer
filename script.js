3f86614a7b7e6060a7aca3a85b9a279eb8ff5bf2
Kenan Ibrović <kenan.ibrovic94@gmail.com>
2017-03-27 00:27:34 +0200

    made some progress with wiki api

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
 
-	function search(){
-		$getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&continue=gsroffset%7C%7C&generator=search&gsrsearch=Irma&gsrlimit=10&gsroffset=10&gsrprop=snippet%7Csectiontitle", function(data){
-			$(".wiki").html(JSON.stringify(data));
-		});
-	}
 
-	$.ajax({
-		type: "GET",
-		url: "https://en.wikipedia.org/w/api.php?action=query&format=json&continue=gsroffset%7C%7C&generator=search&gsrsearch=Irma&gsrlimit=10&gsroffset=10&gsrprop=snippet%7Csectiontitle",
-		success: function(data, jqXHR){
-			console.log(data);
-		}
+$(".button").on("click", function() {
+		var keyWord ="text";
+		var linkAPI = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + keyWord + "&callback=?";
+		//$(".result").html("Result");
+		$.getJSON(linkAPI, function(json) {
+			$(".wiki").html(JSON.stringify(json));
+		});
 	});
+
+	
+	
 
 });
