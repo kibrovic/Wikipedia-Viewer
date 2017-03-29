9a3edd9ad69f7e9288e0104953eda78d600f0fae
Kenan Ibrović <kenan.ibrovic94@gmail.com>
2017-03-29 19:54:15 +0200

    search app done

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 $(document).ready(function(){
-
-
-$(".button").on("click", function() {
-		var keyWord ="text";
-		var linkAPI = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + keyWord + "&callback=?";
-		//$(".result").html("Result");
-		$.getJSON(linkAPI, function(json) {
-			$(".wiki").html(JSON.stringify(json));
-		});
-	});
-
 	
+		//press enter
+	$("#search-box").keypress(function(key){
+		if (key.which==13){
+			var value = $( this ).val();
+			$(".wiki").html("");
+
+			//ajax
+			$.ajax({
+				url: "https://en.wikipedia.org/w/api.php",
+				data: {
+					"action": "query",
+					"format": "json",
+					"prop": "extracts",
+					"indexpageids": 1,
+					"generator": "search",
+					"exchars": "400",
+					"exlimit": "max",
+					"exintro": 1,
+					"gsrsearch": value,
+				},
+				dataType: "jsonp",
+				success: function(data){
+
+					for (var count=0; count<data.query.pageids.length; count++){
+						var index=data.query.pageids[count];
+
+						var titleResult = '<div  class="title" style="width:auto; height:auto; Background:#2a2a2a"><a target="_blank" href="https://en.wikipedia.org/wiki/'+data.query.pages[index].title+'">'+data.query.pages[index].title+'</a></div>';
+						document.querySelector(".wiki").insertAdjacentHTML('beforeend', titleResult);
+
+						var textResult = '<div class="text"><a target="_blank" href="https://en.wikipedia.org/wiki/'+data.query.pages[index].title+'">'+data.query.pages[index].extract+'</a></div>';
+						document.querySelector(".wiki").insertAdjacentHTML('beforeend', textResult);
+
+						
+						//$(".wiki").html(data.query.pages[index].title); // test if it works*/
+					}
+				}
+			}); //end ajax
+			
+
+		} //end if
+
+		
+	});
+ 
 	
 
 });
