a961a5374485993089479a510c38d871af96f849
Kenan Ibrović <kenan.ibrovic94@gmail.com>
2017-07-12 12:59:18 +0200

    change-of-style

--- script.js
+++ script.js
@@ -27,16 +27,24 @@ $(document).ready(function(){
 						var index=data.query.pageids[count];
 
 						var titleResult = '<div  class="title"><a target="_blank" href="https://en.wikipedia.org/wiki/'+data.query.pages[index].title+'">'+data.query.pages[index].title+'</a></div>';
-						document.querySelector(".wiki").insertAdjacentHTML('beforeend', titleResult);
+						//document.querySelector(".result").insertAdjacentHTML('beforeend', titleResult);
 
 						var textResult = '<div class="text"><a target="_blank" href="https://en.wikipedia.org/wiki/'+data.query.pages[index].title+'">'+data.query.pages[index].extract+'</a></div>';
-						document.querySelector(".wiki").insertAdjacentHTML('beforeend', textResult);
+						//document.querySelector(".result").insertAdjacentHTML('beforeend', textResult);
 
-						
-						//$(".wiki").html(data.query.pages[index].title); // test if it works*/
+						var result = '<div class="result hidden">'+ titleResult +'<br>'+ textResult +'</div>';
+						document.querySelector(".wiki").insertAdjacentHTML('beforeend', result);
+
+						$(".hidden").fadeIn(700);
+
+						// hide it then use jquery fade in effect
+						//$(".wiki").html(data.query.pages[index].title); // test if it works
 					}
 				}
 			}); //end ajax
+			
+			
+
 			
 
 		} //end if
